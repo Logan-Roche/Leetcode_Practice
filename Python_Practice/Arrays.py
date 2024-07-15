e86b88567ad660a11e5e49bff213b7c55a8c113c
Logan Roche <Ljroche04@gmail.com>
2024-07-14 23:23:26 -0700

    Python Practice: Arrays

--- Python_Practice/Arrays.py
+++ Python_Practice/Arrays.py
@@ -17,3 +17,60 @@ print(arr)
 # Indexing an array is constant
 arr[0] = 0
 arr[3] = 0
+
+# Initlizing arr of size n with eefault value of 1
+
+n = 5
+
+arr = [1] * n
+
+print(arr)
+print(len(arr))
+
+# Careful: -1 is not out of bounds, it's the last value
+arr = [1, 2, 3]
+print(arr[-1])
+print(arr[-2])
+
+# Sublists (aka Slicing)
+arr = [1, 2, 3, 4, 5]
+
+print(arr[1:3])
+
+# Unpacking arrays into variables
+a, b , c = [1, 2, 3]
+
+print(a, b, c)
+
+# Loop through arrays
+nums = [1, 2, 3]
+
+for i in range(len(nums)):
+    print(nums[i])
+
+# Without Indexing
+for n in nums:
+    print(n)
+
+# With index and value
+for index, value in enumerate(nums):
+    print(index , value)
+print()
+
+
+# loops through multiple arrays simultaneously with unpacking
+nums1 = [1, 3, 5]
+nums2 = [2, 4, 6]
+
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+print()
+
+# Reversing
+nums = [1, 2, 3]
+
+nums.reverse()
+print(nums)
+
+
+
